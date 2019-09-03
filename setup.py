32ac658a3b261db44eff51a5ca09aa3952715c09
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2019-09-03 14:25:34 +0200

    flake8

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 from setuptools import setup, find_packages
 
+
 def readme():
     with open('README.md') as f:
         return f.read()
 
-setup(name = "ezdata",
-    version = 0.9,
-      description = "A Sandbox for simplistic column based data framework",
-    long_description = readme(),
-    author = "Morgan Fouesneau",
-    author_email = "",
-    url = "https://github.com/mfouesneau/ezdata",
-    packages = find_packages(),
-    classifiers=[
-      'Development Status :: 3 - Alpha',
-      'Intended Audience :: Science/Research',
-      'Operating System :: OS Independent',
-      'Programming Language :: Python',
-      'Topic :: Scientific/Engineering :: Astronomy'
-      ],
-    zip_safe=False
-)
+
+setup(name="ezdata",
+      version=0.9,
+      description="A Sandbox for simplistic column based data framework",
+      long_description=readme(),
+      author="Morgan Fouesneau",
+      author_email="",
+      url="https://github.com/mfouesneau/ezdata",
+      packages=find_packages(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Astronomy'
+        ],
+      zip_safe=False)
