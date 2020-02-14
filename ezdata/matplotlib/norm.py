bdb31bd7192b93a707f0efce69aaf14e01e54e55
Morgan Fouesneau <mfouesneau@users.noreply.github.com>
2020-02-14 09:21:28 +0100

    Midpoint norm

--- ezdata/matplotlib/norm.py
+++ ezdata/matplotlib/norm.py
@@ -85,3 +85,16 @@ class Arcsinh(DummyNorm):
     def _inverse_transform(self, value, out=None):
         delta = np.arcsinh(self.vmax) - np.arcsinh(self.vmin)
         return np.sinh(delta * value + np.arcsinh(self.vmin))
+    
+    
+class MidpointNormalize(colors.Normalize):
+    """ Normalize to a central point """
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        # I'm ignoring masked values and all kinds of edge cases to make a
+        # simple example...
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y))
