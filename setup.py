3074a457e4b15f3edabfa92232fbd1493881cc43
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2020-02-27 10:45:00 +0100

    version update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name="ezdata",
-      version=0.95,
+      version=0.96,
       description="A Sandbox for simplistic column based data framework",
       long_description=readme(),
       author="Morgan Fouesneau",
