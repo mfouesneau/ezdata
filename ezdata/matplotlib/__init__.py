ef424d76428ee423c36fce2cf123f33b2fb3f92f
Morgan Fouesneau <mfouesneau@users.noreply.github.com>
2020-03-04 15:40:14 +0100

    added legend import

--- ezdata/matplotlib/__init__.py
+++ ezdata/matplotlib/__init__.py
@@ -1,4 +1,5 @@
 from .light_minimal_theme import light_minimal
 from .helpers import is_colorbar, label_subplots
 from .colors import generate_cmap_from_colors
+from .legend import add_image_legend
 from . import colors
