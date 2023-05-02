edde8baebd0fa1cd4d70e6a73aba54412e6d98e8
Morgan Fouesneau <mfouesneau@users.noreply.github.com>
2023-05-02 20:41:19 +0200

    Update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name="ezdata",
-      version=0.99,
+      version=1.0,
       description="A Sandbox for simplistic column based data framework",
       long_description=readme(),
       author="Morgan Fouesneau",
